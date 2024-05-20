b47b704be54c3ea9d9dc0d240446bcaee9641cf0
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-19 19:15:14 -0500

    feat(project): :lipstick: add Tailwind config styles, and globals.css for challenge

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				'light-green': 'hsl(148, 38%, 91%)',
+				'green': 'hsl(169, 82%, 27%)',
+				'red': 'hsl(0, 66%, 56%)',
+				'medium-gray': 'hsl(186, 15%, 59%)',
+				'dark-gray': 'hsl(187, 24%, 22%)',
+			},
+			colors: {
+				'light-green': 'hsl(148, 38%, 91%)',
+				'green': 'hsl(169, 82%, 27%)',
+				'red': 'hsl(0, 66%, 56%)',
+				'medium-gray': 'hsl(186, 15%, 59%)',
+				'dark-gray': 'hsl(187, 24%, 22%)',
+			}
+		},
 	},
 	plugins: [],
 }
